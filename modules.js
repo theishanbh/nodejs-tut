aba0fee6c106c7a1b9894abf37a1b7e78e457260
ishanbh <theishanbh@gmail.com>
2022-02-06 01:35:38 +0530

    require and imports

--- modules.js
+++ modules.js
@@ -1,3 +1,7 @@
 const xyz = require('./people')
 
-console.log(xyz)
+const {people, ages} = require('./people')
+
+console.log(xyz, people, ages)
+// does not give access to varaibles in xyz if modules.exports not used
+// console.log(people);
--- people.js
+++ people.js
@@ -1,1 +1,6 @@
 const people  = ['yoshi', 'ryu', 'chun-li', 'mario'];
+const ages = [20, 25, 26, 27]
+
+// console.log(people);
+
+module.exports = {ages, people}
